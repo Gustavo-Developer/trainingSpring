2f3405303935c11f56761f22c03d3aee51a885c1
Gustavo <srto.mendes@gmail.com>
2024-11-03 19:15:14 -0300

    implements order entity

--- src/main/java/com/mendesscript/course/entities/User.java
+++ src/main/java/com/mendesscript/course/entities/User.java
@@ -2,6 +2,8 @@ package com.mendesscript.course.entities;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.*;
@@ -20,6 +22,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
 
@@ -71,6 +76,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -83,4 +92,5 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 }
